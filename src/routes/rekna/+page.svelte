c101337803565366f8795a9655307517c4b38d48
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2024-12-24 22:41:38 +0100

    Create +page.svelte

---
+++ src/routes/rekna/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    let number = 0;
+
+    function countPlus() {
+        number++
+    }
+    function countMinus() {
+        number--
+    }
+    function countReset() {
+        number = 0
+    }
+</script>
+<h1>{number}</h1>
+<button onclick="{countPlus}"> <h1>Klicka mig för (+)</h1></button>
+<br>
+<br>
+<button onclick="{countMinus}"> <h1>Klicka mig för (-)</h1></button>
+<br>
+<br>
+<button onclick="{countReset}"> <h1>Klicka mig för (Reset)</h1></button>
+<br>
+<br>
+<style>
+    button{
+        background-color: aquamarine;
+    }
+    h1{
+        color: cadetblue;
+    }
+</style>
